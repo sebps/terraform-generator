596eea4df76d60d1538fa601a24775a0d4eefb80
sebpsdev <sebpsdev@gmail.com>
2021-05-02 13:38:00 +0200

    Clean code

--- commands/variable.go
+++ commands/variable.go
@@ -1,9 +1,7 @@
 package commands
 
 import (
-	"github.com/sebps/terraform-generator/util"
 	"github.com/spf13/cobra"
-	"strings"
 )
 
 type Variable struct {
@@ -35,27 +33,6 @@ func (v *Variable) GetFlagCompletion(flag string) func(*cobra.Command, []string,
 
 func (v *Variable) GetNoonCompletion() func(*cobra.Command, []string, string) ([]string, cobra.ShellCompDirective) {
 	return nil
-}
-
-func (c *Configuration) typCompletion(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
-	var results []string
-	var dir string
-
-	dir = "."
-	if c.Dir != "" {
-		dir = c.Dir
-	} else {
-		dir = "."
-	}
-
-	configurations := util.ParseConfigurations(dir)
-	for _, c := range configurations {
-		if strings.HasPrefix(c.Name, toComplete) {
-			results = append(results, c.Name)
-		}
-	}
-
-	return results, cobra.ShellCompDirectiveDefault
 }
 
 func (v *Variable) typeCompletion(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
